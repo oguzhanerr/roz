09161507e63c53463fce42ccd4a6adc4e59c6479
Oguzhan <oguzhanerr@gmail.com>
2024-08-02 01:16:50 +0200

    added data schemas

---
+++ R/data_schema.R
@@ -0,0 +1,14 @@
+# Data_schema
+
+# expected columns
+expected_columns <- c("poi_name", "poi_subtype", "latitude", "longitude", "is_connected", "has_electricity")
+
+# expected types for each column
+expected_types <- list(
+  poi_name = "character",
+  poi_subtype = list(type = "character", values = c("school", "hospital", "post office")),
+  latitude = "numeric",
+  longitude = "numeric",
+  is_connected = "logical",
+  has_electricity = "logical"
+)
