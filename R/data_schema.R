16a26dfff149ebf43e9ac9b9a14fd548b92eeca6
Oguzhan <oguzhanerr@gmail.com>
2024-08-02 17:01:00 +0200

    Updated data schema

--- R/data_schema.R
+++ R/data_schema.R
@@ -1,14 +1,14 @@
-# Data_schema
+.onLoad <- function(libname, pkgname) {
+  # Define expected columns
+  expected_columns <<- c("poi_name", "poi_subtype", "latitude", "longitude", "is_connected", "has_electricity")
 
-# expected columns
-expected_columns <- c("poi_name", "poi_subtype", "latitude", "longitude", "is_connected", "has_electricity")
-
-# expected types for each column
-expected_types <- list(
-  poi_name = "character",
-  poi_subtype = list(type = "character", values = c("school", "hospital", "post office")),
-  latitude = "numeric",
-  longitude = "numeric",
-  is_connected = "logical",
-  has_electricity = "logical"
-)
+  # Define expected types for each column
+  expected_types <<- list(
+    poi_name = "character",
+    poi_subtype = list(type = "character", values = c("school", "hospital", "post office")),
+    latitude = "numeric",
+    longitude = "numeric",
+    is_connected = "logical",
+    has_electricity = "logical"
+  )
+}
